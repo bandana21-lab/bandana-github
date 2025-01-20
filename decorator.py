cab99b4b1fc1183bdee7e877c3292e27af919e2a
bandana <bandanapaija588@gmail.com>
2025-01-20 08:54:19 +0545

    decorator

---
+++ decorator.py
@@ -0,0 +1,22 @@
+from comm import com_decorator
+@com_decorator
+def name():
+    """This is name function."""
+    pass
+@com_decorator
+def grade():
+    """This is grade function."""
+    pass
+@com_decorator
+def age():
+    """This is age function"""
+    pass
+@com_decorator
+def course():
+    """This is course function"""
+    pass
+
+name()
+grade()
+age()
+course()
