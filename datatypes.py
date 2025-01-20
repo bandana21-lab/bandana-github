6381d58a641577201684a975cca775e8fdb4574d
bandana <bandanapaija588@gmail.com>
2025-01-20 08:53:14 +0545

    data types

---
+++ datatypes.py
@@ -0,0 +1,21 @@
+# python datatypes
+# int-10(numer)
+# primitive datatype(value bata detect huni)
+# datatypes-int, float, string
+a=10
+print(type(a))
+# float
+b=20.22
+print(type(b))
+# string
+name = "bandana"
+print(type(name))
+poem='''
+baby shark do do do
+baby shark
+'''
+print(poem)
+print(type(poem))
+
+# reference
+# tuple,set,list,dictionary
