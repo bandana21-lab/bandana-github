18928a513b78ca9134dbb5427324a63c22e9fa22
bandana <bandanapaija588@gmail.com>
2025-01-20 08:52:00 +0545

    conditional

---
+++ conditional_statement.py
@@ -0,0 +1,20 @@
+# if,elif,else 
+# day=input("Enter a day: ")
+# if day=="saturday":
+#     print("xya aaja ta padhna jana paryo"
+# num=int(input("Enter a number"))
+# if num/2==0:
+#     print( "it is even")
+# else:
+#/'
+# num=int(input("enter a number: "))
+# if (num %5)==0 and (num%3)==0:
+#     print("It is divisible by 5 and 3")
+# else:
+#     print("It is not divisible by 5 and 3")
+
+
+
+# Conditional_statement
+# they_are if, elif, else 
+# statement if condition else statement
