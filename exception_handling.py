4e10f93e24791043e5ef55f723e392f585cfe522
bandana <bandanapaija588@gmail.com>
2025-01-21 18:31:42 +0545

    exception handling

---
+++ exception_handling.py
@@ -0,0 +1,12 @@
+# def division(x,y):
+#     try:
+#         return x/y
+#     except(ZeroDivisionError):
+#         return "seccond number must not be zero"
+#     except(TypeError):
+#         return "shouldnot be string"
+
+# number1=int(input("Enter first number: "))
+# number2=int(input("Enter second number: "))
+# d=division(number1,number2)
+# print(d)
