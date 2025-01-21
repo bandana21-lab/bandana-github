dfe7e445c7d818e7c7ebb14b4ddc93058e35436f
bandana <bandanapaija588@gmail.com>
2025-01-21 18:42:34 +0545

    string

---
+++ string_manipulation.py
@@ -0,0 +1,62 @@
+# text="we are learning python"
+# print(text[16:22])
+# print(text[:6])
+# name="bandana"
+# for i in range(len(name)):
+#     print(i,name[i])
+
+# task string modifier
+
+
+# name="___###***Tripti***###___"
+# format_one=name.strip("_")
+# format_two=format_one.strip("#")
+# format_three=format_two.strip("*")
+# print(format_three)
+    
+# t=name.strip('_').strip("#")
+
+# text="we are learning python"
+# print(text.replace("python","react"))
+
+# url="https://www.youtube.com"
+# print(url.split("."))
+
+# find()=position value 
+# text="we are learning python"
+# # print(text.find("e"))
+# print(text.count("e"))
+
+# startsWith snd endsWith
+# user_gmail=input("Enter your email: ")
+# if user_gmail.endswith(".edu.np"):
+#     print("your email is valid")
+# else:
+#     print("invalid email")
+
+# formatted string 
+
+# # task
+# name="##########sachin##########"
+# symbols="*******"
+# email
+# password
+# emailpasswordloginfailed
+# upper() lower()
+# text="i am bandana"
+# print(text.upper())
+# removing white space-strip()
+# name="    bandanan   "
+# print(name.strip())
+# split is used to split the sentence into array
+# text="i am home"
+# print(text.split())
+# print(text.find("am"))
+# print(text.endswith("home"))
+    
+
+# format 
+full_name="bandana"
+age=21
+result="my name is {0} and i am {1} years old".format(full_name,age)
+print(result)
