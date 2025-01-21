3c8300b70a784befe9484c5e33884870af15de44
bandana <bandanapaija588@gmail.com>
2025-01-21 18:37:04 +0545

    function

---
+++ function.py
@@ -0,0 +1,30 @@
+# def diff():
+    # x=20
+    # y=10
+    # print(x-y)
+# diff()
+
+# first_name=input("Enter firts name: ")
+# last_name=input("Enter last name: ")
+# def info(fname,lname):
+#     print(fname,lname)
+# info(first_name,last_name)
+
+#requirement
+#first_numnber and second_number
+#additon
+# subtraction
+# division 
+# multiplication
+# choice
+    
+# function-block of code which executes whenever w ecall it 
+# function declaration 
+# function definition
+# in python function definition and functin declaration are done at the same time 
+# function call 
+  
+# signature function 
+# def sum(x,y)
+
+# def sum(x,y): x,y id parameter
